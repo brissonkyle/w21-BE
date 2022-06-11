d96fa529b6f8b1aaff8c8b9c1caa829cdf9e91a4
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-11 13:12:47 -0600

    Pushing test Code

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from dbcreds import *
 import sys
 from flask import jsonify
 from helpers.db_helpers import run_query
+# from helpers.db_helpers import run_query
 
 app = Flask(__name__)
-conn = None
-cursor = None
+
 
 conn = mariadb.connect(
                     user=user,
@@ -20,8 +20,16 @@ conn = mariadb.connect(
 @app.get('/api/animals')
 def animals_get():
     # TODO : DB SELECT
-    animal_list = []
-    return jsonify(animal_list), 200
+    run_query('SELECT * FROM animal')
+    resp = []
+    for animal in animal_list:
+        an_obj : {}
+        an_obj['animalId'] = animal[0]
+        an_obj['animalName'] = animal[1]
+        an_obj['imageUrl'] = animal[2]
+        resp.append(an_obj)
+    animal_list = ['bird']
+    return jsonify(resp), 200
 
 
 @app.post('/api/animals')
@@ -35,6 +43,7 @@ def animals_post():
         return jsonify('Missing required argument imageUrl'), 422
     # TODO : ERROR CHECK THE VALUES FOR THE ARGUMENTS 
     # TODO : DB WRITE
+    run_query('INSERT INTO animal (name, imageUrl) VALUES (?,?)', [animalName, imageUrl])
     return jsonify('Animal added'), 201
 
 if len(sys.argv) > 1:
