c073a20fc4c112e0600d55108d05107e23eb61be
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-11 10:46:50 -0600

    ADDED APP LAUNCH

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request
 import mariadb
 from dbcreds import *
+import sys
 from flask import jsonify
 from helpers.db_helpers import run_query
 
@@ -34,3 +35,21 @@ def animals_post():
         return jsonify('Missing required argument imageUrl'), 422
     # TODO : ERROR CHECK THE VALUES FOR THE ARGUMENTS 
     # TODO : DB WRITE
+    return jsonify('Animal added'), 201
+
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+else:
+    print('Missing required arguments')
+    exit
+
+if mode == 'testing':
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+elif mode == 'production':
+    import bjoern
+    bjoern.run(app, '0.0.0.0', 5005)
+else :
+    print('Mode must be in testing/production')
+    exit()
